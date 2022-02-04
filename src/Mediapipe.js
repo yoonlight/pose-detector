3b6e502c267886de53ff088dd47f973761b16e70
yoonlight <yoonlight12@gmail.com>
2022-02-04 22:41:28 +0900

    refactor: split Mediapipe into Canvas and rest

--- src/Mediapipe.js
+++ src/Mediapipe.js
@@ -1,23 +1,59 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { Camera } from "@mediapipe/camera_utils";
-import { Pose, POSE_CONNECTIONS, VERSION } from "@mediapipe/pose";
+import { Pose, POSE_CONNECTIONS, VERSION, Results } from "@mediapipe/pose";
 import { drawLandmarks, drawConnectors } from "@mediapipe/drawing_utils";
 
-function Mediapipe() {
-  const inputVideo = useRef();
-  const outputCanvas = useRef();
-  const pose = new Pose({
-    locateFile: (file) => {
-      return `https://cdn.jsdelivr.net/npm/@mediapipe/pose@${VERSION}/${file}`;
-      //   return `/pose/${file}`;
-    },
+/**
+ *
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {Results} results
+ */
+const draw = (ctx, results) => {
+  if (!results) return;
+  const cWidth = ctx.canvas.width;
+  const cHeight = ctx.canvas.height;
+  ctx.save();
+  ctx.clearRect(0, 0, cWidth, cHeight);
+  ctx.drawImage(results.segmentationMask, 0, 0, cWidth, cHeight);
+
+  ctx.drawImage(results.image, 0, 0, cWidth, cHeight);
+
+  ctx.globalCompositeOperation = "source-over";
+  drawConnectors(ctx, results.poseLandmarks, POSE_CONNECTIONS, {
+    color: "#00FF00",
+    lineWidth: 4,
+  });
+  drawLandmarks(ctx, results.poseLandmarks, {
+    color: "#FF0000",
+    lineWidth: 2,
+  });
+  ctx.restore();
+};
+
+const Canvas = (props) => {
+  const { results } = props;
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    /** @type {HTMLCanvasElement} */
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    draw(ctx, results);
   });
 
+  return <canvas ref={canvasRef} {...props} width={1280} height={720} />;
+};
+
+function Mediapipe() {
+  const [results, setResults] = useState(null);
+  const inputVideo = useRef();
+
   useEffect(() => {
-    let canvasElement = outputCanvas.current;
-    let canvasCtx = canvasElement.getContext("2d");
-    canvasElement.height = window.innerHeight
-    canvasElement.width = window.innerWidth
+    const pose = new Pose({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/pose@${VERSION}/${file}`;
+        //   return `/pose/${file}`;
+      },
+    });
     pose.setOptions({
       modelComplexity: 1,
       smoothLandmarks: true,
@@ -30,38 +66,8 @@ function Mediapipe() {
     pose.initialize();
 
     function onResults(results) {
-      if (!results.poseLandmarks) {
-        return;
-      }
-
-      canvasCtx.save();
-      canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-      canvasCtx.drawImage(
-        results.segmentationMask,
-        0,
-        0,
-        canvasElement.width,
-        canvasElement.height
-      );
-
-      canvasCtx.drawImage(
-        results.image,
-        0,
-        0,
-        canvasElement.width,
-        canvasElement.height
-      );
-
-      canvasCtx.globalCompositeOperation = "source-over";
-      drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
-        color: "#00FF00",
-        lineWidth: 4,
-      });
-      drawLandmarks(canvasCtx, results.poseLandmarks, {
-        color: "#FF0000",
-        lineWidth: 2,
-      });
-      canvasCtx.restore();
+      if (!results.poseLandmarks) return;
+      setResults(results);
     }
     const videoEl = inputVideo.current;
     const camera = new Camera(videoEl, {
@@ -70,11 +76,11 @@ function Mediapipe() {
       },
     });
     camera.start();
-  });
+  }, []);
   return (
     <div>
       <video ref={inputVideo} playsInline style={{ display: "none" }}></video>
-      <canvas ref={outputCanvas} width="1280px" height="720px"></canvas>
+      <Canvas results={results}></Canvas>
     </div>
   );
 }
